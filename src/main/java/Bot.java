a17da7e27c9a8acd1a042900d8c0d6001b8b7ce3
Nayalash Mohammad <1nayalashm@gmail.com>
2019-11-21 21:08:55 -0500

    fixed token

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -5,7 +5,6 @@ import net.dv8tion.jda.api.entities.Activity;
 
 import javax.security.auth.login.LoginException;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 
 public class Bot {
@@ -15,13 +14,14 @@ public class Bot {
 
     public static void main(String[] args) throws LoginException, FileNotFoundException, IOException {
 
-        FileReader reader = new FileReader("token.txt");
-        char[] tokenData = new char[59];
+//        FileReader reader = new FileReader("token.txt");
+//        char[] tokenData = new char[59];
+//
+//        int response = reader.read(tokenData);
+//        token = new String(tokenData);
+// Alternative
 
-        int response = reader.read(tokenData);
-        token = new String(tokenData);
-
-        bot = new JDABuilder(token).build();
+        bot = new JDABuilder(System.getenv("token")).build();
         bot.getPresence().setStatus(OnlineStatus.ONLINE);
         bot.getPresence().setActivity(Activity.listening("Expenses"));
 
